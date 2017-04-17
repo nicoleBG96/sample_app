3956df35818abb842257152e7e5160ed236bb444
Nicole <nikybg96@gmail.com>
2017-04-16 20:36:55 -0400

    Add a Static Pages controller

---
+++ app/views/static_pages/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @static_page, :id, :name, :email, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  resources :static_pages
   root 'application#hello'
 end
